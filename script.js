65bf5edbfb2435676b188947166acce971b80048
chanjeff520 <chanjeff520@gmail.com>
2022-09-28 15:26:02 -0400

    add index.html and script.jss

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+create variable for paper, rock and scissor.
+create variable for computor choice and human choice.
+create variable for computer win and human win and draw
+human choise equals to prompt r,p,s (0,1,2)
+make random generator for computor choice(0,1,2)
+if humanchoice=[0] and comchoice=[1] then you lose (+1 to computer win)
+if humanchoice=[0] and comchoice=[2] then you win (+1 to human win)
+if humanchoice=[1] and comchoice=[0] then you win (+1 to human win)
+if humanchoice=[1] and comchoice=[2] then you lose (+1 to computer win)
+if humanchoice=[2] and comchoice=[0] then you lose (+1 to computer win)
+if humanchoice=[2] and comchoice=[1] then you win (+1 to human win)
+else then draw (+1 to draw)
+*/
