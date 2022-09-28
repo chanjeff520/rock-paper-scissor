adbf7ac96854e8f785e0826a7e2795733bb310c1
chanjeff520 <chanjeff520@gmail.com>
2022-09-28 15:51:41 -0400

    add human choices and ai choices

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ if humanchoice=[2] and comchoice=[0] then you lose (+1 to computer win)
 if humanchoice=[2] and comchoice=[1] then you win (+1 to human win)
 else then draw (+1 to draw)
 */
+
+var humanWins, aiWins, ties = 0;
+
+function rockPaperScissor(){
+    var humanChoice;
+    var aiChoice;
+    var rockPaperOrScissor = ["r", "p", "s"];
+
+    humanChoice = prompt("Choice your move Rock(r), Paper(p), Scissor(s). Your choices are r, p and s");
+    aiChoice = rockPaperOrScissor[Math.floor(Math.random() * 3)];
+
+    if(humanChoice == aiChoice){
+        ties += 1;
+        alert("Computer choose"+ aiChoice+". It's a draw");
+    }else if((humanChoice == "r" && aiChoice == "p") || (humanChoice == "p" && aiChoice == "s") 
+                || (humanChoice == "s" && aiChoice == "r")){
+        aiWins = 1;
+    }
+}
