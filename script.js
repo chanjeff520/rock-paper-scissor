43e0c680229723745c4d99fc497d1a95d0ce05d9
chanjeff520 <chanjeff520@gmail.com>
2022-09-28 21:08:29 -0400

    case sensitity does not matter anymore

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /*
 create variable for paper, rock and scissor.
 create variable for computor choice and human choice.
@@ -13,21 +14,35 @@ if humanchoice=[2] and comchoice=[1] then you win (+1 to human win)
 else then draw (+1 to draw)
 */
 
-var humanWins, aiWins, ties = 0;
-
+var humanWins = 0,aiWins= 0,ties =0;
 function rockPaperScissor(){
     var humanChoice;
     var aiChoice;
     var rockPaperOrScissor = ["r", "p", "s"];
+    var playAgain = false;
 
     humanChoice = prompt("Choice your move Rock(r), Paper(p), Scissor(s). Your choices are r, p and s");
+    //humanChoice = humanChoice.toLowerCase;
     aiChoice = rockPaperOrScissor[Math.floor(Math.random() * 3)];
 
     if(humanChoice == aiChoice){
-        ties += 1;
+        ties+=1;
         alert("Computer choose"+ aiChoice+". It's a draw");
     }else if((humanChoice == "r" && aiChoice == "p") || (humanChoice == "p" && aiChoice == "s") 
                 || (humanChoice == "s" && aiChoice == "r")){
-        aiWins = 1;
+        aiWins +=1;
+        alert("Computer choose"+ aiChoice+". You LOSE!");
+    }else if((humanChoice == "p" && aiChoice == "r") || (humanChoice == "s" && aiChoice == "p") 
+                || (humanChoice == "r" && aiChoice == "s")){
+        humanWins += 1;
+        alert("Computer choose"+ aiChoice+". You WIN!!!");
+    }else{
+        alert("Please Choose a valid character(r,p,s)");
+        return;
     }
+    playAgain = confirm("AI wins: " + aiWins +".  Your Wins: " + humanWins + ".  Ties: "+ ties + ". Do you want to play again?");
+    
+    if(playAgain == true) rockPaperScissor();
 }
+
+rockPaperScissor();
